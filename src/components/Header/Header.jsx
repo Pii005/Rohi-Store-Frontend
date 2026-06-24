39ab02a94db7be1db285226e1fa2d602c24ca6c1
pii005 <pilar.flores04@gmail.com>
2026-06-24 20:19:31 -0300

    feat: add responsive header and mobile sidebar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,61 +2,66 @@ import { useState } from "react";
 import './Header.css'
 import { IoMenu } from "react-icons/io5";
 import Sidebar from "../Sidebar/Sidebar";
-import { TbShoppingCart, LuUserRound , BiSearchAlt  } from "react-icons/tb";
-// src\assets\icons\logoRohi.png
+import { TbShoppingCart } from "react-icons/tb";
+import { LuUserRound } from "react-icons/lu";
+import { BiSearchAlt } from "react-icons/bi";// src\assets\icons\logoRohi.png
 
-function HeaderHome() {
+export default function HeaderHome() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    
+
     const toggleMenu = () => {
-        // console.log("Cambiando a " + ismenuOpen);
         setIsMenuOpen(!isMenuOpen);
-       
-        // console.log("Paso a " + (isMenuOpen ? "Abierto" : "Cerrado"));
     };
 
-    // function Himenu() {
-    //     // console.log("Himenu toggle esta - " + (isMenuOpen ? "Abierto" : "Cerrado"));
-    //     toggleMenu();
-    //     if(isMenuOpen) {
-    //         console.log("Abriendo menu");
-    //         return <Sidebar />;
-    //     }
-    // }
-
-    
     return (
         <>
         <header>
             <div className="header-left">
-                {/* Menu y Logo */}
-                {/* <button className="menu-button" onClick={() => toggleMenu()} ><IoMenu className="IoMenu" /></button> */}
-                <img  src="../src/assets/icons/logoRohiBlack.png" className="logoRohiBlack" alt="Logo"  />
-                {/* <h1>{isMenuOpen ? "ABIERTO" : "CERRADO"}</h1>      */}
+                <button
+                    className="menu-button"
+                    onClick={toggleMenu}
+                >
+                    <IoMenu className="IoMenu" />
+                </button>
+
+                <img
+                    src="../src/assets/icons/logoRohiBlack.png"
+                    className="logoRohiBlack"
+                    alt="Logo"
+                />
+
                 <div className="Options-Header">
                     <h5>Amigurumis</h5>
                     <h5>Llaveros</h5>
                     <h5>Accesorios</h5>
                     <h5>Ramos</h5>
                     <h5>Personalizados</h5>
-
                 </div>
-
             </div>
+
             <div className="header-right">
-                {/* Buscador, perfil/login/registro y carrito */}
-                <div className="Options-Header">
-                    <h5><BiSearchAlt /></h5>
-                    <h5><LuUserRound /></h5>
-                    <h5><TbShoppingCart /></h5>
+                <div className="search-bar">
+                    <input
+                        type="text"
+                        placeholder="Buscar..."
+                        className="search-input"
+                    />
+                    <BiSearchAlt className="search-icon" />
                 </div>
+
+                <LuUserRound className="header-icon" />
+                <TbShoppingCart className="header-icon" />
             </div>
         </header>
-       
+
+        {isMenuOpen && (
+            <div className={isMenuOpen ? "sidebar-open" : "sidebar-close"}>
+                <Sidebar />
+            </div>
+        )}
         </>
 
     );
 
 };
 
-export default HeaderHome;
